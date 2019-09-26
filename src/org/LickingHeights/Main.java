890e8836e9bd8d505f4eac19cb80fef461a3852d
Hamza <hamooz7670@gmail.com>
2019-09-26 10:11:02 -0400

    did calculation for axis of symmetry and discriminant

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -3,85 +3,87 @@ package org.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+        // write your code here
 
-       //todo declare variables a,b,c,discriminant axisOfSymmetry,parabolaFacing,x,y
-        double a,b,c,discriminant,axisOfSymmetry,x,y;
+
+        //todo declare variables a,b,c,discriminant axisOfSymmetry,parabolaFacing,x,y
+        double a, b, c, discriminant, axisOfSymmetry, x, y;
         String parabolaFacing;
-       //todo initialize manually
-        a=5;
-        b=3;
-        c=1;
-        
-      //todo do calculations
+        //todo initialize manually
+        a = 5;
+        b = 3;
+        c = 1;
 
-      //todo print results
+        //todo do calculations
+        axisOfSymmetry = -b/(2*a);
+        discriminant = (b*b)-4*(a*c);
+        //todo print results
 
-      //todo not in code check results
+        //todo not in code check results
 
-      //todo initiaize scanner and redo the initialize area
+        //todo initiaize scanner and redo the initialize area
 
-      //todo double check results
+        //todo double check results
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
